ff286bc70dd57eea1144e6e44fdaa90525c4fe10
SamirAllaham <samir.allaham@ocsbstudent.ca>
2021-02-19 15:58:47 +0000

    spaces

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ let age = 17
 function movie () {
   age = document.getElementById('input').value
   age = parseInt(age)
-  if (age >= 17 ) {
+  if (age >= 17) {
     document.getElementById('answer').innerHTML = 'You can see R rated movies'
-  } else if(age >= 13){
+  } else if (age >= 13) {
     document.getElementById('answer').innerHTML = 'You can see PG-13 movies'
-    } else if(age >= 5) {
+  } else if (age >= 5) {
     document.getElementById('answer').innerHTML = 'You can see PG movies'
-    } else {
-  
+  } else {
     document.getElementById('answer').innerHTML = 'You are too young for these movies'
-    }
-    document.getElementById('movie').innerHTML = 'Thank for verifying your age!'
+  }
+  document.getElementById('movie').innerHTML = 'Thank for verifying your age!'
 }
